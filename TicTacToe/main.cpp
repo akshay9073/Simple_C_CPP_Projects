80f94349c7546744118c0f781ee2f7f6f20d767c
akshay_9073 <bhandari.akshay29@gmail.com>
2025-11-07 16:49:05 +0530

    getwinner function added

--- TicTacToe/main.cpp
+++ TicTacToe/main.cpp
@@ -126,3 +126,37 @@ bool cellAlreadyOccupied(int row, int col, string gameBoard[ROWS][COLS])
 {
 	return gameBoard[row][col]!=" ";
 }
+
+string getWinner(string gameBoard[ROWS][COLS])
+{
+	for(int i=0;i<ROWS;i++)
+	{
+		if(gameBoard[i][0]!=" "&& gameBoard[i][0]==gameBoard[i][1] && gameBoard[i][1]==gameBoard[i][2])
+		{
+			return gameBoard[i][0];
+		}
+	}
+	
+	for(int i=0;i<COLS;i++)
+	{
+		if(gameBoard[0][i]!=" "&& gameBoard[0][i]==gameBoard[1][i] && gameBoard[1][i]==gameBoard[2][i])
+		{
+			return gameBoard[0][i];
+		}
+	}
+	
+	if(gameBoard[0][0]!=" "&& gameBoard[0][0]==gameBoard[1][1] && gameBoard[1][1]==gameBoard[2][2])
+		{
+			return gameBoard[0][0];
+		}
+	
+	if(gameBoard[2][0]!=" "&& gameBoard[2][0]==gameBoard[1][1] && gameBoard[1][1]==gameBoard[0][2])
+		{
+			return gameBoard[2][0];
+		}
+	
+		
+		return "";
+}
+
+
