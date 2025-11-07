305e69840cb4e743d3a94801968e54b13e837234
akshay_9073 <bhandari.akshay29@gmail.com>
2025-11-07 08:55:51 +0530

    cellAlreadyOccupied, getUserInput function added

--- TicTacToe/main.cpp
+++ TicTacToe/main.cpp
@@ -9,8 +9,8 @@ const int COLS=3;
 void runGame();
 void initalizeGameBoard(string gameBoard[][COLS]);
 void printCurrentBoard(string gameBoard[][COLS]);
-
-
+void getUserInput(bool xTurn,string gameBoard[][COLS]);
+bool cellAlreadyOccupied(int,int,string gameBoard[][COLS]);
 
 int main()
 {
@@ -34,7 +34,24 @@ void runGame()
 	initalizeGameBoard(gameBoard);
 	
 	printCurrentBoard(gameBoard);
+	
+	while(winner== "")
+	{
+		if(xTurn)
+		{
+			cout<<"It is X's turn"<<endl;
+		}
+		else
+		{
+			cout<<"It is 0's turn"<<endl;
+		}
+		
+		getUserInput(xTurn,gameBoard);
+	}
 }
+
+
+
 
 void initalizeGameBoard(string gameBoard[ROWS][COLS])
 {
@@ -66,4 +83,46 @@ void printCurrentBoard(string gameBoard[ROWS][COLS])
 			}
 		}
 		cout<<endl;	
+}
+
+void getUserInput(bool xTurn,string gameBoard[ROWS][COLS])
+{
+	int row=-1;
+	int col=-1;
+	
+	bool keepAsking=true;
+	
+	while(keepAsking)
+	{
+		cout<<"Please enter the row THEN the column, each from 0, 1, or 2, separated by a space"<<endl;
+		cin>>row;
+		cin>>col;
+		
+		if(row>=0 && col>=0 && row<=2 && col<=2)
+		{
+			if(!cellAlreadyOccupied(row,col,gameBoard))
+			{
+				keepAsking=false;
+			}
+			else
+			{
+				cout<<"That cell is already occupied!!"<<endl;
+			}
+		}
+	}
+	
+	if(xTurn)
+	{
+		gameBoard[row][col]="x";
+		
+	}
+	else
+	{
+		gameBoard[row][col]="o";
+	}
+}
+
+bool cellAlreadyOccupied(int row, int col, string gameBoard[ROWS][COLS])
+{
+	return gameBoard[row][col]!=" ";
 }
